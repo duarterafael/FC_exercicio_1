b9a7d1b425db19b41c708c82e097d340c6ba26c5
duarterafael <rafaelbatistaduarte@gmail.com>
2024-05-24 20:07:26 -0300

    Teste commit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Helloooo, World!");
         Filme filme = new Filme(1, "Casa blanca", 1970, "Rafael", 120);
         Console.WriteLine(filme.Titulo);
         filme.ExibirDetalhes();
